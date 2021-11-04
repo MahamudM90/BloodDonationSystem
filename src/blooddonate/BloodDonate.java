70fca14e471d7d35cba7920620b492be217fa42e
Mahamud Hasan <mahamud15-10467@diu.edu.bd>
2021-11-04 14:15:33 +0600

    first commit

---
+++ src/blooddonate/BloodDonate.java
@@ -0,0 +1,14 @@
+
+package blooddonate;
+
+
+public class BloodDonate {
+
+
+    public static void main(String[] args) {
+        CoverPage obj= new CoverPage();
+        obj.setVisible(true);
+        
+    }
+    
+}
